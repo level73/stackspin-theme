71e93ac4236188f772f170ab9308f97811a14bfb
runovaart <67725075+runovaart@users.noreply.github.com>
2022-01-06 21:27:05 +0100

    footer update

--- footer.php
+++ footer.php
@@ -16,54 +16,52 @@
 		</div><!-- #primary -->
 	</div><!-- #content -->
 
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'stackspin' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-		<div class="site-info">
+		<div class="site-footer__main limit-width">
 			<div class="site-name">
 				<?php if ( has_custom_logo() ) : ?>
 					<div class="site-logo"><?php the_custom_logo(); ?></div>
-				<?php else : ?>
-					<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
+					<?php if ( get_bloginfo( 'description' ) ) : ?>
 						<?php if ( is_front_page() && ! is_paged() ) : ?>
-							<?php bloginfo( 'name' ); ?>
-						<?php else : ?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+							<p class="site-name__description"><?php bloginfo( 'description' ); ?></p>
 						<?php endif; ?>
 					<?php endif; ?>
 				<?php endif; ?>
 			</div><!-- .site-name -->
-			<div class="powered-by">
-				<?php
-				printf(
-					/* translators: %s: WordPress. */
-					esc_html__( 'Proudly powered by %s.', 'stackspin' ),
-					'<a href="' . esc_url( __( 'https://wordpress.org/', 'stackspin' ) ) . '">WordPress</a>'
-				);
-				?>
-			</div><!-- .powered-by -->
-
-		</div><!-- .site-info -->
+			<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
+			<form class="site-footer__form">
+				<h3 class="site-footer__form-header">Subscribe to our newsletter</h3>
+				<p class="site-footer__form-desc">
+					The latest news, articles, and resources, sent to your inbox weekly.
+				</p>
+				<div class="site-footer__form-wrapper">
+					<input type="text" class="site-footer__form-input" placeholder="Enter your email" />
+					<input type="submit" class="site-footer__form-submit" value="Subscribe" />
+				</div>
+			</form>
+		</div>
+		<div class="site-info limit-width">
+			<label class="site-info__copyright">© 2021 Stackspin. All rights reserved.</label>
+			<?php if ( has_nav_menu( 'footer' ) ) : ?>
+				<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'stackspin' ); ?>" class="footer-navigation">
+					<ul class="footer-navigation-wrapper">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'footer',
+								'items_wrap'     => '%3$s',
+								'container'      => false,
+								'depth'          => 1,
+								'link_before'    => '<span>',
+								'link_after'     => '</span>',
+								'fallback_cb'    => false,
+							)
+						);
+						?>
+					</ul><!-- .footer-navigation-wrapper -->
+				</nav><!-- .footer-navigation -->
+			<?php endif; ?>
+		</div> <!-- .site-info -->
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
